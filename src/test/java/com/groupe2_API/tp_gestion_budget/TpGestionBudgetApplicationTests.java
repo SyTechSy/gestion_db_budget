5b04a7850de2a66c7e21ed544a537772ced68845
unknown <sy.diakaridia@odc.local>
2023-08-04 10:57:21 +0000

    notre projet buget

---
+++ src/test/java/com/groupe2_API/tp_gestion_budget/TpGestionBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe2_API.tp_gestion_budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpGestionBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
