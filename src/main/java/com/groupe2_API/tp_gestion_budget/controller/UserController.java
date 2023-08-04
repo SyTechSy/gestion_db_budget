88f0c50c44c7c5e9c07720dfeb9facda0d9c37f3
ibrahim10sy <ibrahim10sy@gmail.com>
2023-08-04 15:16:44 +0000

    commit apres la methode modifier

--- src/main/java/com/groupe2_API/tp_gestion_budget/controller/UserController.java
+++ src/main/java/com/groupe2_API/tp_gestion_budget/controller/UserController.java
@@ -4,10 +4,7 @@ import com.groupe2_API.tp_gestion_budget.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -23,6 +20,16 @@ public class UserController {
             return new ResponseEntity<>("Inscription fait avec Succès", HttpStatus.OK);
         } else {
             return new ResponseEntity<>("Compte existe déja", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/modifier")
+    public ResponseEntity<Object> modificationUser(@RequestBody User user) {
+        User verificationUser = userService.modifierUser(user);
+        if (verificationUser != null) {
+            return new ResponseEntity<>("Modification fait avec Succès", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Modification echouer", HttpStatus.NOT_FOUND);
         }
     }
 
