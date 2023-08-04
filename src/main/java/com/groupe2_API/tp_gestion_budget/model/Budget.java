438bcbac7c54ae04fcd196b4fb02c2e5fc877d5d
unknown <sy.diakaridia@odc.local>
2023-08-04 17:30:15 +0000

    Le commencement de budget

--- src/main/java/com/groupe2_API/tp_gestion_budget/model/Budget.java
+++ src/main/java/com/groupe2_API/tp_gestion_budget/model/Budget.java
@@ -1,5 +1,35 @@
 package com.groupe2_API.tp_gestion_budget.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+import java.util.Date;
+
+@Data
+@Entity
 public class Budget {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(nullable = false)
+    private long idBudget;
+
+    @NotNull(message = "Le champs montant de doit pas être vide")
+    @Min(value = 5000, message = "Désole vous montant doivent être superieur ou egale a 5000 Fcfa")
+    @Column(nullable = false)
+    private int montant;
+
+
+    @NotNull(message = "Le champs montant de doit pas être vide")
+    @Column(nullable = false)
+    private Date dateDebut;
+
+    @NotNull(message = "Le champs montant de doit pas être vide")
+    @Column(nullable = false)
+    private Date dateFin;
+
+
 }
