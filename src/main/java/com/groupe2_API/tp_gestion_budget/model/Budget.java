66f19f9b50290bc987324b898b9e3095068fb874
Isac <isacdiakite3@gmail.com>
2023-08-09 17:43:20 +0000

    Commit

--- src/main/java/com/groupe2_API/tp_gestion_budget/model/Budget.java
+++ src/main/java/com/groupe2_API/tp_gestion_budget/model/Budget.java
@@ -1,14 +1,13 @@
 package com.groupe2_API.tp_gestion_budget.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -18,6 +17,7 @@ public class Budget {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     @Column(nullable = false)
 
     private long idBudget;
@@ -26,6 +26,7 @@ public class Budget {
     @Min(value = 5000, message = "Désole vous montant doivent être superieur ou egale a 5000 Fcfa")
     @Column(nullable = false)
     private double montant;
+
 
     @NotNull(message = "Le champs date debut ne doit pas être vide")
     @Column(nullable = false)
@@ -39,35 +40,16 @@ public class Budget {
 
     @OneToMany(mappedBy = "budget", cascade = CascadeType.ALL,orphanRemoval = true)
     @JsonIgnore
-    private List<Depense> depenses = new ArrayList<>();
-
-    @ManyToOne
-    @JsonIgnoreProperties(value = {"budgets"})
-    @JoinColumn(name = "idUser", nullable = false)
-    private User user;
+    private List<Depense> depenses;
 
 
     /*
     @DateTimeFormat(pattern = "dd-MM-yyyy")
-=======
-    @Column(nullable = false)
-    private double montantRestant;
-    // ==============================  =======================
-
-    //@DateTimeFormat(pattern = "dd-MM-yyyy")
->>>>>>> 9a0267b3d03b5819a419d3fb222f64c425df3c2e
     @Column(name = "date")
     private Date date;
-    /*@DateTimeFormat(pattern = "dd-MM-yyyy")
-    @Column(name = "dateD")
-    private Date dateDebut;
 
-   // @DateTimeFormat(pattern = "dd-MM-yyyy")
-    @Column(name = "dateF")
-    private Date dateFin;*/
 
     //========================Relation entre budget et categorie====================
-
 
     @OneToOne
     @JsonIgnore
@@ -75,11 +57,8 @@ public class Budget {
     private Categorie categorie;
     //========================Relation entre budget et user====================
     @ManyToOne
-<<<<<<< HEAD
     @JoinColumn(name = "idUser")
     private User user;*/
-
-
 
 
 }
