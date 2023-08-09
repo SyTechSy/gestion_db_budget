f81686823af7b7afeecff1eb7f9827338c10a530
unknown <sy.diakaridia@odc.local>
2023-08-09 11:21:13 +0000

    verificxation des emails

--- src/main/java/com/groupe2_API/tp_gestion_budget/service/DepenseService.java
+++ src/main/java/com/groupe2_API/tp_gestion_budget/service/DepenseService.java
@@ -4,8 +4,10 @@ package com.groupe2_API.tp_gestion_budget.service;
 import com.groupe2_API.tp_gestion_budget.model.Budget;
 import com.groupe2_API.tp_gestion_budget.model.Categorie;
 import com.groupe2_API.tp_gestion_budget.model.Depense;
+import com.groupe2_API.tp_gestion_budget.model.EmailDetails;
 import com.groupe2_API.tp_gestion_budget.repository.BudgetRepository;
 import com.groupe2_API.tp_gestion_budget.repository.DepenseRepository;
+import com.groupe2_API.tp_gestion_budget.repository.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,10 @@ public class DepenseService {
     BudgetService budgetService;
     @Autowired
     BudgetRepository budgetRepository;
+
+    @Autowired
+    EmailService emailService;
+
 
    /* public Depense creer(Depense depense){
 
@@ -47,9 +53,15 @@ public class DepenseService {
             double montantRestant = montantBudget - montantDepense;
             budget.setMontant(montantRestant);
             budgetRepository.save(budget);
+            // envoyer emaill a chaque depense
+            String msg = "Votre budget est de " + budget.getMontant() + " Fcfa, maintenant il vous reste " + montantRestant;
+            EmailDetails details = new EmailDetails(depense.getUser().getEmail(),msg,"Détaille de votre depense");
+            emailService.sendSimpleMail(details);
 
             return "Dépense créée avec succès. Montant restant dans le budget : " + montantRestant;
         }
+
+
     }
 
 
